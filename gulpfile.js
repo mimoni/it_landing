3865eed39e04651769ccbd0fc08eea5aa420d9c3
Eugene Parshukov <ev.klit@gmail.com>
2017-10-12 05:08:57 +0300

    add autoprefixer and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,8 @@ const sass = require('gulp-sass');
 const spritesmith = require('gulp.spritesmith');
 const rimraf = require('rimraf');
 const rename = require('gulp-rename');
-
-
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
 
 /* -------- Server  -------- */
@@ -33,7 +33,13 @@ gulp.task('templates:compile', function buildHTML() {
 /* ------------ Styles compile ------------- */
 gulp.task('styles:compile', function () {
   return gulp.src('source/styles/main.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(sourcemaps.write())
     .pipe(rename('main.min.css'))
     .pipe(gulp.dest('build/css'));
 });
